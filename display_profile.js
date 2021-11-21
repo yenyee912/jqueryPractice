26fbb0e1050a58155884c39ebe998ea876fe03a1
yenyee912 <fuyazi1997@gmail.com>
2021-11-21 23:14:22 +0800

    Add files via upload

---
+++ display_profile.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(() => {
+  // display data from session storage
+  $("#email").text(sessionStorage.getItem("email"));
+  $("#phone").text(sessionStorage.getItem("phone"));
+  $("#zip").text(sessionStorage.getItem("zip"));
+  $("#dob").text(sessionStorage.getItem("dob"));
+
+  $("#back").click(() => {
+    history.go(-1)
+
+  }); // end of click()
+
+}); // end of ready()
